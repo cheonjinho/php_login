e2e5450c58422254fff1aba17f4d4f67b4cd4b10
jin <sr_soou@naver.com>
2017-04-08 23:48:36 +0900

    rewrite

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,8 +49,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['users/registration'] = 'users/registration';
-$route['users/login'] = 'users/login';
 $route['default_controller'] = 'users/login';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
