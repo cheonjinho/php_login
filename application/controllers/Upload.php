b95c8f913eaf65a743961f033770a125e85d6ac4
jin <sr_soou@naver.com>
2017-04-11 11:33:09 +0900

    php settiing

--- application/controllers/Upload.php
+++ application/controllers/Upload.php
@@ -15,7 +15,7 @@ class Upload extends CI_Controller {
 
 	public function do_upload()
 	{
-		$config['upload_path']          = './uploads/';
+		$config['upload_path']          = './application/uploads/';
 		$config['allowed_types']        = 'gif|jpg|png';
 		$config['max_size']             = 0;
 		$config['max_width']            = 0;
--- application/views/upload_form.php
+++ application/views/upload_form.php
@@ -8,10 +8,7 @@
 
 <?php echo form_open_multipart('upload/do_upload');?>
 
-<input type="file" name="userfile" size="20" />
-
-<br /><br />
-
+<input type="file" name="userfile" size="20" /><br>
 <input type="submit" value="upload" />
 
 </form>
